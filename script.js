84b3c40593e6637d3b135869edd7ef433c90df31
Anmol Purwar <112842731+anmol-2004@users.noreply.github.com>
2024-11-08 00:56:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function imgSlider(anything){
+    document.querySelector('.pepsi').src = anything;
+}
+function changebgColor(coLor){
+    document.querySelector('.sec').style.background = coLor;
+}
